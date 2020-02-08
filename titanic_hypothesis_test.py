cb0709ce0b8e0f770d8c6b13cb57e6289e438d0c
Aidan-Jared <46360020+Aidan-Jared@users.noreply.github.com>
2020-02-08 13:00:30 -0700

    Personal data science project

---
+++ titanic_hypothesis_test.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import numpy as np
+from itertools import combinations
+
+def plot_hist(df, title, color):
+    df.hist(figsize=(12, 5), sharey=True, grid=False, color=color, alpha=0.5)
+    plt.suptitle(title, size=18, weight='bold')
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    # importing and cleaning the data
+    df = pd.read_csv("data/train.csv")
+    df.drop(["Cabin", "Embarked", "Ticket"], axis = 1, inplace = True)
+    df.dropna(inplace = True)
+    df["Sex"].replace({"male" : 0, "female" : 1}, inplace = True)
+
+    #setting the signifcance level
+    alpha = .05 / len(df["PassengerId"])
+
+    # eda
+    # plot_hist(df, "Titanic Data", "g")
+
+    # seperating male from female
+    df_male = df[df["Sex"] == 0].copy()
+    df_female = df[df["Sex"] == 1].copy()
+    p = len(df_male) / len(df)
+
+    # binomal test
+    p_val = stats.binom_test(df_male['Survived'].sum(), n = len(df_male['Survived']), p = p, alternative = 'less')
+
+    if p_val <= alpha:
+        print("Gender was an important factor in surviving the titanic")
+    else:
+        print("Gender was not an important factor in surviving the titanic")
+
+    fig = plt.figure()
+    df_male["Survived"].hist(normed=True, label="Male", color='g', alpha=0.3)
+    df_female["Survived"].hist(normed=True, label="Female", color='r', alpha=0.3)
+    plt.ylabel("Probabliity Density")
+    plt.xlabel('Survived')
+    plt.legend()
+    plt.show()
