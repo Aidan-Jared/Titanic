d253865ba65924c87534935b23e6097d6cdf0579
Aidan-Jared <46360020+Aidan-Jared@users.noreply.github.com>
2020-02-14 13:37:05 -0700

    updated hyperparams and added in validation

--- src/titanic_ml.py
+++ src/titanic_ml.py
@@ -4,6 +4,7 @@ h2o.init()
 from h2o.estimators.gbm import H2OGradientBoostingEstimator
 from h2o.grid.grid_search import H2OGridSearch
 import pandas as pd
+import math
 
 if __name__ == "__main__":
 
@@ -18,6 +19,7 @@ if __name__ == "__main__":
 
     # Putting data into h2o
     train = h2o.H2OFrame(df)
+    train, valid = train.split_frame([.8], seed=62)
     test = h2o.H2OFrame(df_test)
     factor_col = ["Pclass", "Sex", "Parch", "Survived"]
     for i in factor_col:
@@ -28,27 +30,26 @@ if __name__ == "__main__":
     del x[0]
 
     # setting the parameters to try out
-    params = {
-        'ntrees' : [30,40,50,60,70],
-        'nbins_cats': [2,4,8],
-        'learn_rate' : [1,.1],
-        'sample_rate' : [.9, 1],
-        'col_sample_rate' : [.9,.8, 1],
-        'seed' : [42],
-        'stopping_rounds' : [10],
-        'stopping_tolerance' : [1e-5],
-        'stopping_metric' : ['logloss'],
-        'balance_classes': [True, False]
-        }
+    params = { "ntrees": [10000],
+                'max_depth' : list(range(1,30,1)),
+                'sample_rate': [x/100. for x in range(20,101)],
+                'col_sample_rate' : [x/100. for x in range(20,101)],
+                'col_sample_rate_per_tree': [x/100. for x in range(20,101)],
+                'col_sample_rate_change_per_level': [x/100. for x in range(90,111)],
+                'min_rows': [2**x for x in range(0,int(math.log(train.nrow,2)-1)+1)],
+                'nbins': [2**x for x in range(4,11)],
+                'nbins_cats': [2**x for x in range(4,13)],
+                'min_split_improvement': [0,1e-8,1e-6,1e-4],
+                'histogram_type': ["UniformAdaptive","QuantilesGlobal","RoundRobin"]}
     
     # grid search on GBM
     grid = H2OGridSearch(H2OGradientBoostingEstimator, params)
-    grid.train(x=x, y=y, training_frame=train)
+    grid.train(x=x, y=y, training_frame=train, validation_frame=valid, max_runtime_secs=3600)
 
     # getting the model
-    model = grid.get_grid(sort_by='logloss')[0]
-    perf = model.model_performance(test)
-    pred = model.predict(test)
+    best_model = grid.get_grid(sort_by='auc', decreasing=True)[0]
+    perf = best_model.model_performance(test)
+    pred = best_model.predict(test)
     print(perf.auc())
     print(pred.head())
     print("done")
